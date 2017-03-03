cedaffbf5fb0d4c1c738b3859de8c29f044bee4e
zhanghua54 <zhanghua54@wanda.cn>
2017-03-03 16:20:25 +0800

    delete useless svg files

---
+++ js/popup.js
@@ -0,0 +1,12 @@
+
+
+// var notification = chrome.notifications.create("abc", { 
+// 	type: "basic", 
+// 	iconUrl: 'images/icon.png', 
+// 	title: 'Notification Demo', 
+// 	message: 'Merry Christmas' 
+// }, function() {}); 
+
+// setTimeout(function() { 
+// 	chrome.notifications.clear("abc", function() {}); 
+// }, 5000); 
