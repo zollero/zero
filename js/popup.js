343e7c81805cbca9d6a2d32adadeed2e4ad173b4
zollero <corona7@163.com>
2017-03-05 01:40:01 +0800

    add todo

--- js/popup.js
+++ js/popup.js
@@ -1,46 +1,62 @@
-
-
-// var notification = chrome.notifications.create("abc", { 
-// 	type: "basic", 
-// 	iconUrl: 'images/icon19.png', 
-// 	title: 'Notification Demo', 
-// 	message: 'Merry Christmas',
-// 	buttons: [{
-// 		title: 'I got this',
-// 		iconUrl: 'images/icon19.png'
-// 	}, {
-// 		title: 'Dismiss',
-// 		iconUrl: 'images/icon38.png'
-// 	}]
-// }, function() {}); 
-
-// setTimeout(function() {
-// 	chrome.notifications.clear("abc", function() {}); 
-// }, 5000); 
+/*!
+ * z-todo  - https://github.com/zollero/todo-chrome
+ * Version - 1.0.0
+ * Licensed under the MIT license - http://opensource.org/licenses/MIT
+ *
+ * Copyright (c) 2017 zollero
+ */
 
 
 (function(window, document) {
 	
-	var todoList = [{
-		status: 'on',	//on, done, undone
-		message: 'Call honey',
-		setNotice: true,	//false, true
-		noticeTime: '2017-03-05 10:20'
-	}, {
-		status: 'done',
-		message: 'Write something',
-		setNotice: false
-	}, {
-		status: 'undone',
-		message: 'Study python',
-		setNotice: true,
-		noticeTime: '2017-02-01 10:20',
-	}];
+	// var todoList = [{
+	// 	status: 'on',	//on, done, undone
+	// 	message: 'Call honey',
+	// 	setNotice: true,	//false, true
+	// 	noticeTime: '2017-03-05 10:20'
+	// }, {
+	// 	status: 'done',
+	// 	message: 'Write something',
+	// 	setNotice: false
+	// }, {
+	// 	status: 'undone',
+	// 	message: 'Study python',
+	// 	setNotice: true,
+	// 	noticeTime: '2017-02-01 10:20',
+	// }];
 
 	var $listContainer = document.getElementById('todo-list');
+	var $addTaskInput = document.getElementById('add-task');
 
-	createLists(todoList);
-	// createLists();
+
+	$addTaskInput.addEventListener('keyup', function(e) {
+		var value = e.target.value;
+		if(e.keyCode === 13 && value.trim().length > 0) {
+			console.log('Type enter');
+			var newTodo = {
+				status: 'on',
+				message: value,
+				setNotice: false,
+				noticeTime: ''
+			};
+			e.target.value = '';
+			saveNewTodo(newTodo);
+			refreshList();
+		}
+	}, false);
+
+	function saveNewTodo(newTodo) {
+		var todos = JSON.parse(localStorage.getItem('todos'));
+		if (!todos) todos = [];
+		todos.push(newTodo);
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}
+
+	function refreshList() {
+		createLists(JSON.parse(localStorage.getItem('todos')));
+	}
+
+	refreshList();
 
 	function createLists(lists) {
 			var $todoListHtml = '';
@@ -64,7 +80,7 @@
 						'<div class=\"status\">' +
 							'<div class=\"icon-container\">' +
 								'<i class=\"icon\">' +
-									(list.status === 'done' ? '✓' : '') +
+									(list.status === 'done' ? '&#10003' : '') +
 								'</i>' +
 							'</div>' + 
 						'</div>' +
