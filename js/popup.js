53031acded6a9268cb6a025bda66d9346b617f20
zollero <corona7@163.com>
2017-03-05 00:48:25 +0800

    done css style

--- js/popup.js
+++ js/popup.js
@@ -19,10 +19,80 @@
 // }, 5000); 
 
 
-var todoList = [{
-	status: 'on',	//on, done, undone
-	message: 'Call honey',
-	setNotice: false,	//false, true
-	noticeTile: '2017-03-05 10:20',
+(function(window, document) {
 	
-}]
+	var todoList = [{
+		status: 'on',	//on, done, undone
+		message: 'Call honey',
+		setNotice: true,	//false, true
+		noticeTime: '2017-03-05 10:20'
+	}, {
+		status: 'done',
+		message: 'Write something',
+		setNotice: false
+	}, {
+		status: 'undone',
+		message: 'Study python',
+		setNotice: true,
+		noticeTime: '2017-02-01 10:20',
+	}];
+
+	var $listContainer = document.getElementById('todo-list');
+
+	createLists(todoList);
+	// createLists();
+
+	function createLists(lists) {
+			var $todoListHtml = '';
+		if (lists && lists.length > 0) {
+			lists.forEach(function(v, i) {
+				$todoListHtml += createList(v);
+			});
+		} else {
+			//no todos
+			$todoListHtml += createTip();
+		}
+		$listContainer.innerHTML = $todoListHtml;
+	}
+
+	function createList(list) {
+		var listHtml = '';
+		if (typeof list === 'object') {
+			listHtml = listHtml + 
+				'<li class=\"li\">' + 
+					'<div class=\"list ' + list.status + '\">' +
+						'<div class=\"status\">' +
+							'<div class=\"icon-container\">' +
+								'<i class=\"icon\">' +
+									(list.status === 'done' ? '✓' : '') +
+								'</i>' +
+							'</div>' + 
+						'</div>' +
+						'<div class=\"content\" title=\"' + list.message + '\">' +
+							list.message +
+						'</div>' +
+						'<div class=\"notice\">' +
+							'<i class=\"iconfont ' + (list.setNotice ? '' : 'disabled') + ' icon-notice\"></i>' +
+						'</div>' +
+					'</div>' +
+					'<div class=\"actions\">' +
+						'<div class=\"action\">' +
+							'<i class=\"iconfont icon-notice\"></i>' +
+						'</div>' +
+						'<div class=\"action\">' +
+							'<i class=\"iconfont icon-delete\"></i>' +
+						'</div>' +
+					'</div>' +
+				'</li>';
+		}
+		return listHtml;
+	}
+
+	function createTip(message) {
+		if (!message) message = 'There are no todos.';
+		return '<li class=\"li\">' +
+					'<div class=\"tip\">' + message + '</div>' +
+				'</li>';
+	}
+
+}(window, document));
