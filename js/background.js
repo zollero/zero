2d4d8b1e73751dced131941b67bebff902caca8f
zollero <corona7@163.com>
2017-03-04 22:49:31 +0800

    rewrite css

--- js/background.js
+++ js/background.js
@@ -1,3 +1,12 @@
+var notification = chrome.notifications.create("abc", { 
+	type: "basic", 
+	iconUrl: 'images/icon19.png', 
+	title: 'Notification Demo', 
+	message: 'Merry Christmas' 
+}, function() {}); 
 
+setTimeout(function() { 
+	chrome.notifications.clear("abc", function() {}); 
+}, 5000); 
 
 // alert('1111')
--- js/popup.js
+++ js/popup.js
@@ -2,11 +2,27 @@
 
 // var notification = chrome.notifications.create("abc", { 
 // 	type: "basic", 
-// 	iconUrl: 'images/icon.png', 
+// 	iconUrl: 'images/icon19.png', 
 // 	title: 'Notification Demo', 
-// 	message: 'Merry Christmas' 
+// 	message: 'Merry Christmas',
+// 	buttons: [{
+// 		title: 'I got this',
+// 		iconUrl: 'images/icon19.png'
+// 	}, {
+// 		title: 'Dismiss',
+// 		iconUrl: 'images/icon38.png'
+// 	}]
 // }, function() {}); 
 
-// setTimeout(function() { 
+// setTimeout(function() {
 // 	chrome.notifications.clear("abc", function() {}); 
 // }, 5000); 
+
+
+var todoList = [{
+	status: 'on',	//on, done, undone
+	message: 'Call honey',
+	setNotice: false,	//false, true
+	noticeTile: '2017-03-05 10:20',
+	
+}]
